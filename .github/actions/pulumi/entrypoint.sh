3922c3dca8d6fbd3bb576b0af24f6ccdda5585ca
Timothy Myers <tim@ip-10-1-10-18.ec2.internal>
2019-12-10 22:28:50 -0700

    delete lines

--- .github/actions/pulumi/entrypoint.sh
+++ .github/actions/pulumi/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh -l
 set -ex
 
-# export PULUMI_CI_SYSTEM="GitHub"
-# export PULUMI_CI_BUILD_ID=
-# export PULUMI_CI_BUILD_TYPE=
-# export PULUMI_CI_BUILD_URL=
-# export PULUMI_CI_PULL_REQUEST_SHA="$GITHUB_SHA"
-
 yarn
 cd infrastructure
 helm init --client-only
