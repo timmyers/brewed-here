85bf641309b7dcf15799b43bd2c7d9182fd10ca8
Timothy Myers <tim@ip-10-1-10-18.ec2.internal>
2019-12-10 21:51:10 -0700

    Add soem env vars

--- .github/actions/pulumi/entrypoint.sh
+++ .github/actions/pulumi/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
+export PULUMI_CI_SYSTEM="GitHub"
+export PULUMI_CI_BUILD_ID=
+export PULUMI_CI_BUILD_TYPE=
+export PULUMI_CI_BUILD_URL=
+export PULUMI_CI_PULL_REQUEST_SHA="$GITHUB_SHA"
+
 yarn
 cd infrastructure
 helm init --client-only
