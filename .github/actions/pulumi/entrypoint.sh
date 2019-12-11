e4d7d5b6176c46835e69a97f4ccce27597714456
Timothy Myers <tim@ip-10-1-10-18.ec2.internal>
2019-12-10 22:24:30 -0700

    conditionals

--- .github/actions/pulumi/entrypoint.sh
+++ .github/actions/pulumi/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -l
 set -ex
 
-export PULUMI_CI_SYSTEM="GitHub"
-export PULUMI_CI_BUILD_ID=
-export PULUMI_CI_BUILD_TYPE=
-export PULUMI_CI_BUILD_URL=
-export PULUMI_CI_PULL_REQUEST_SHA="$GITHUB_SHA"
+# export PULUMI_CI_SYSTEM="GitHub"
+# export PULUMI_CI_BUILD_ID=
+# export PULUMI_CI_BUILD_TYPE=
+# export PULUMI_CI_BUILD_URL=
+# export PULUMI_CI_PULL_REQUEST_SHA="$GITHUB_SHA"
 
 yarn
 cd infrastructure
 helm init --client-only
 helm repo update
-pulumi preview -s cluster
+pulumi $OPERATION -s cluster
