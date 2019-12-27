44043de6cba0c3645a05cd9d694ffd9d1fcd6396
Timothy Myers <tim@Timothys-MacBook-Pro.local>
2019-12-27 15:43:31 -0700

    ugh

--- .github/actions/expo/entrypoint.sh
+++ .github/actions/expo/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -l
 set -ex
 
-yarn global add expo-cli
 cd app
+cat app.json | jq '.expo.sdkVersion' -r > /tmp/expo-sdk-version
+yarn global add gulp-cli turtle-cli@$TURTLE_VERSION
+turtle setup:$PLATFORM --sdk-version `cat /tmp/expo-sdk-version`
+
+yarn global add expo-cli
 yarn
 expo login -u $EXPO_CLI_USERNAME
 expo publish
