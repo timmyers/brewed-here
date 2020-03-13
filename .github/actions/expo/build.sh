4dfc5e19582a4e82805a7defb1241c849a2d6b4b
Timothy Myers <timmyers09@gmail.com>
2020-03-12 20:24:49 -0600

    signing updates

--- .github/actions/expo/build.sh
+++ .github/actions/expo/build.sh
@@ -5,13 +5,13 @@ cd app
 cat app.json | jq '.expo.sdkVersion' -r > /tmp/expo-sdk-version
 yarn global add gulp-cli turtle-cli@$TURTLE_VERSION
 turtle setup:$PLATFORM --sdk-version `cat /tmp/expo-sdk-version`
+
+echo $EXPO_ANDROID_KEYSTORE_BASE64 > expo-project.jks.base64
+base64 --decode expo-project.jks.base64 > expo-project.jks
+
 turtle build:android \
+  --keystore-path ./expo-project.jks \
+  --keystore-alias $EXPO_ANDROID_KEYSTORE_ALIAS \
   --type app-bundle \
   --public-url https://expo.brewedhere.co/android-index.json \
   -o brewed-here.aab
-ls
-
-# yarn global add expo-cli
-# yarn
-# expo login -u $EXPO_CLI_USERNAME
-# expo publish
