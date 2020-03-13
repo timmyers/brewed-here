5eea37af2ffd56a11c36cf583c2633c2e7f3a041
Timothy Myers <timmyers09@gmail.com>
2020-03-12 21:46:30 -0600

    doctl purge

--- .github/actions/expo/publish.sh
+++ .github/actions/expo/publish.sh
@@ -5,4 +5,5 @@ cd app
 yarn global add expo-cli
 yarn
 expo export --public-url https://expo.brewedhere.co
-s3cmd --access_key=$SPACES_ACCESS_KEY_ID --secret_key=$SPACES_SECRET_ACCESS_KEY --host=sfo2.digitaloceanspaces.com --host-bucket="%(bucket)s.sfo2.digitaloceanspaces.com" --acl-public sync dist s3://brewed-here
+s3cmd --access_key=$SPACES_ACCESS_KEY_ID --secret_key=$SPACES_SECRET_ACCESS_KEY --host=sfo2.digitaloceanspaces.com --host-bucket="%(bucket)s.sfo2.digitaloceanspaces.com" --acl-public sync dist/* s3://brewed-here
+doctl compute cdn flush 4d7ca47f-1ff5-4614-b216-d4f5db0748e4 --files '[*]'
