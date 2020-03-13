8b30ec031062b0ac98b33e1326d194b638dc4746
Timothy Myers <timmyers09@gmail.com>
2020-03-13 13:25:49 -0600

    Try again

--- .github/actions/expo/publish.sh
+++ .github/actions/expo/publish.sh
@@ -4,6 +4,9 @@ set -ex
 cd app
 yarn global add expo-cli
 yarn
+
+yarn semantic-relase --dry-run
+
 sed -i "s/ANDROID_MAPS_API_KEYS/$ANDROID_MAPS_API_KEYS/g" app.json
 expo export --public-url https://expo.brewedhere.co
 s3cmd --access_key=$SPACES_ACCESS_KEY_ID --secret_key=$SPACES_SECRET_ACCESS_KEY --host=sfo2.digitaloceanspaces.com --host-bucket="%(bucket)s.sfo2.digitaloceanspaces.com" --acl-public sync dist/* s3://brewed-here
