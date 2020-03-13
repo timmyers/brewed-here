1c0fa7876775e572b23cfa044b911722343cfbd4
Timothy Myers <timmyers09@gmail.com>
2020-03-13 17:02:21 -0600

    fix: build-app add debug line

--- .github/actions/expo/build.sh
+++ .github/actions/expo/build.sh
@@ -6,6 +6,7 @@ cat app.json | jq '.expo.sdkVersion' -r > /tmp/expo-sdk-version
 yarn global add gulp-cli turtle-cli@$TURTLE_VERSION
 turtle setup:$PLATFORM --sdk-version `cat /tmp/expo-sdk-version`
 
+pwd && ls && cat app.json
 sed -i "s/ANDROID_MAPS_API_KEYS/$ANDROID_MAPS_API_KEYS/g" app.json
 
 echo $EXPO_ANDROID_KEYSTORE_BASE64 > expo-project.jks.base64
