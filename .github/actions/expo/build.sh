0077846ece5f330d6fcef222eab52b1f29ece604
Timothy Myers <timmyers09@gmail.com>
2020-03-13 17:22:52 -0600

    fix: build-app get rid of extra sed

--- .github/actions/expo/build.sh
+++ .github/actions/expo/build.sh
@@ -7,7 +7,6 @@ yarn global add gulp-cli turtle-cli@$TURTLE_VERSION
 turtle setup:$PLATFORM --sdk-version `cat /tmp/expo-sdk-version`
 
 pwd && ls && cat app.json
-sed -i "s/ANDROID_MAPS_API_KEYS/$ANDROID_MAPS_API_KEYS/g" app.json
 
 echo $EXPO_ANDROID_KEYSTORE_BASE64 > expo-project.jks.base64
 base64 --decode expo-project.jks.base64 > expo-project.jks
