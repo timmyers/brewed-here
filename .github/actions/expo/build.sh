a55d51edf7280b79b543179d8aaa2caf56c01cd9
Timothy Myers <timmyers09@gmail.com>
2020-03-13 21:49:30 -0600

    fix: build-app new image

--- .github/actions/expo/build.sh
+++ .github/actions/expo/build.sh
@@ -2,9 +2,9 @@
 set -ex
 
 cd app
-cat app.json | jq '.expo.sdkVersion' -r > /tmp/expo-sdk-version
-yarn global add gulp-cli turtle-cli@$TURTLE_VERSION
-turtle setup:$PLATFORM --sdk-version `cat /tmp/expo-sdk-version`
+# cat app.json | jq '.expo.sdkVersion' -r > /tmp/expo-sdk-version
+# yarn global add gulp-cli turtle-cli@$TURTLE_VERSION
+# turtle setup:$PLATFORM --sdk-version `cat /tmp/expo-sdk-version`
 
 echo $EXPO_ANDROID_KEYSTORE_BASE64 > expo-project.jks.base64
 base64 --decode expo-project.jks.base64 > expo-project.jks
@@ -12,6 +12,7 @@ base64 --decode expo-project.jks.base64 > expo-project.jks
 sed -i "s/ANDROID_MAPS_API_KEYS/$ANDROID_MAPS_API_KEYS/g" app.json
 
 ls -al .
+cat app.json
 
 turtle build:android \
   --config ./app.json \
