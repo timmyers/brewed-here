8ba4237599bd0b1ddd1a7f982849c65802946c93
Timothy Myers <timmyers09@gmail.com>
2020-03-13 22:06:24 -0600

    fix: build-app fix image

--- .github/actions/expo/build.sh
+++ .github/actions/expo/build.sh
@@ -2,6 +2,7 @@
 set -ex
 
 cd app
+
 # cat app.json | jq '.expo.sdkVersion' -r > /tmp/expo-sdk-version
 # yarn global add gulp-cli turtle-cli@$TURTLE_VERSION
 # turtle setup:$PLATFORM --sdk-version `cat /tmp/expo-sdk-version`
