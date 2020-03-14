e92e6232fe929ea8da36a8e99c56e3340225e295
Timothy Myers <timmyers09@gmail.com>
2020-03-13 22:27:47 -0600

    fix: build-app add chown

--- .github/actions/expo/build.sh
+++ .github/actions/expo/build.sh
@@ -6,7 +6,6 @@ cd app
 # cat app.json | jq '.expo.sdkVersion' -r > /tmp/expo-sdk-version
 # yarn global add gulp-cli turtle-cli@$TURTLE_VERSION
 # turtle setup:$PLATFORM --sdk-version `cat /tmp/expo-sdk-version`
-
 echo $EXPO_ANDROID_KEYSTORE_BASE64 > expo-project.jks.base64
 base64 --decode expo-project.jks.base64 > expo-project.jks
 
