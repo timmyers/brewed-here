cf101e7c57076c7561c518bebd23bf4da777ca9b
Timothy Myers <timmyers09@gmail.com>
2020-03-13 23:13:17 -0600

    fix: build-app quotes

--- .github/actions/expo/build.sh
+++ .github/actions/expo/build.sh
@@ -13,4 +13,5 @@ sed -i "s/ANDROID_MAPS_API_KEYS/$ANDROID_MAPS_API_KEYS/g" app.json
 
 turtle build:android --keystore-path="./expo-project.jks" --keystore-alias="$EXPO_ANDROID_KEYSTORE_ALIAS" --type="app-bundle" --public-url="https://expo.brewedhere.co/android-index.json" --output="brewed-here.aab" .
 
+
 export TURTLE_BUILD=true
