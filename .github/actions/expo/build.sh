6685acacb36f01b2f751782dfd805910333324a9
Timothy Myers <timmyers09@gmail.com>
2020-03-18 23:56:05 -0600

    fix: build-app config

--- .github/actions/expo/build.sh
+++ .github/actions/expo/build.sh
@@ -10,6 +10,6 @@ sed -i "s/ANDROID_MAPS_API_KEYS/$ANDROID_MAPS_API_KEYS/g" app.json
 
 yarn global add gulp-cli turtle-cli@$TURTLE_VERSION
 turtle setup:$PLATFORM
-turtle build:android --keystore-path=./expo-project.jks --keystore-alias=$EXPO_ANDROID_KEYSTORE_ALIAS --public-url=https://expo.brewedhere.co/android-index.json --type=app-bundle --output=brewed-here.aab --config $GITHUB_WORKSPACE/app/app.json
+turtle build:android --keystore-path=$GITHUB_WORKSPACE/app/expo-project.jks --keystore-alias=$EXPO_ANDROID_KEYSTORE_ALIAS --public-url=https://expo.brewedhere.co/android-index.json --type=app-bundle --output=brewed-here.aab --config $GITHUB_WORKSPACE/app/app.json
 
 echo "::set-env name=TURTLE_BUILD::true"
--- buildAndroid.js
+++ buildAndroid.js
@@ -1,4 +1,6 @@
 const execa = require('execa');
+const { google } = require('googleapis')
+const fs = require('fs');
 
 const stdin = process.stdin;
 const inputChunks = [];
@@ -23,9 +25,97 @@ stdin.on('end', async () => {
   })
 
   if (doBuild) {
-    const { exitCode } = await execa('/build.sh', { stdio: 'inherit' })
-    process.exit(exitCode);
+    // const { exitCode } = await execa('/build.sh', { stdio: 'inherit' })
+    // if (exitCode !== 0) {
+    //   process.exit(exitCode);
+    // }
+
+    try {
+      console.log('Uploading brewed-here.aab to google play...')
+
+      fs.writeFileSync('google.json', Buffer.from(process.env.ANDROID_UPLOAD_KEY_BASE64, 'base64'));
+      const auth = new google.auth.GoogleAuth({
+        keyFile: './google.json',
+        scopes: ['https://www.googleapis.com/auth/androidpublisher']
+      });
+      const authClient = await auth.getClient()
+
+      const uploader = google.androidpublisher({
+        version: 'v3',
+        auth: authClient,
+      })
+
+      const edit = await uploader.edits.insert({
+        packageName: 'co.brewedhere.brewedhere',
+      })
+
+      const aabResponse = await uploader.edits.bundles.upload({
+        editId: edit.data.id,
+        packageName: 'co.brewedhere.brewedhere',
+        media: {
+          mimeType: 'application/octet-stream',
+          body: fs.createReadStream('brewed-here.aab')
+        },
+      })
+
+      const update = await uploader.edits.tracks.update({
+        editId: edit.data.id,
+        packageName: 'co.brewedhere.brewedhere',
+        track: 'internal',
+      })
+
+      const commit = await uploader.edits.commit({
+        editId: edit.data.id,
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   process.exit(0)
 });
+
+
+(async () => {
+  try {
+      console.log('Uploading brewed-here.aab to google play...')
+
+      fs.writeFileSync('google.json', Buffer.from(process.env.ANDROID_UPLOAD_KEY_BASE64, 'base64'));
+      const auth = new google.auth.GoogleAuth({
+        keyFile: './google.json',
+        scopes: ['https://www.googleapis.com/auth/androidpublisher']
+      });
+      const authClient = await auth.getClient()
+
+      const uploader = google.androidpublisher({
+        version: 'v3',
+        auth: authClient,
+      })
+
+      const edit = await uploader.edits.insert({
+        packageName: 'co.brewedhere.brewedhere',
+      })
+
+      const aabResponse = await uploader.edits.bundles.upload({
+        editId: edit.data.id,
+        packageName: 'co.brewedhere.brewedhere',
+        media: {
+          mimeType: 'application/octet-stream',
+          body: fs.createReadStream('brewed-here.aab')
+        },
+      })
+
+      const update = await uploader.edits.tracks.update({
+        editId: edit.data.id,
+        packageName: 'co.brewedhere.brewedhere',
+        track: 'internal',
+      })
+
+      const commit = await uploader.edits.commit({
+        editId: edit.data.id,
+      })
+
+    } catch (err) {
+      console.log(err)
+    }
+})()
