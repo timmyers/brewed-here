4a245f9e772d4a8261b90627cc5dc751f8d1deec
Timothy Myers <timmyers09@gmail.com>
2020-03-18 21:01:34 -0600

    fix: build-app config

--- .github/actions/expo/build.sh
+++ .github/actions/expo/build.sh
@@ -10,7 +10,6 @@ echo $EXPO_ANDROID_KEYSTORE_BASE64 > expo-project.jks.base64
 base64 --decode expo-project.jks.base64 > expo-project.jks
 
 sed -i "s/ANDROID_MAPS_API_KEYS/$ANDROID_MAPS_API_KEYS/g" app.json
-pwd
 
 turtle build:android --keystore-path=./expo-project.jks --keystore-alias=$EXPO_ANDROID_KEYSTORE_ALIAS --public-url=https://expo.brewedhere.co/android-index.json --type=app-bundle --output=brewed-here.aab --config /brewed-here/app/app.json
 
