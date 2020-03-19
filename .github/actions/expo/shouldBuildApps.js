4c94b6f15433517b3d0ce17d133834d7a225aec1
Timothy Myers <timmyers09@gmail.com>
2020-03-19 16:45:59 -0600

    fix(ci): fix read path

--- .github/actions/expo/shouldBuildApps.js
+++ .github/actions/expo/shouldBuildApps.js
@@ -5,7 +5,8 @@ const fs = require('fs');
   const artifactClient = artifact.create();
   const downloadResponse = await artifactClient.downloadArtifact('BUILD_APPS', '.', { createArtifactFolder: false });
 
-  const buildApps = JSON.parse(fs.readFileSync(`${downloadResponse.downloadPath}/buildApps.json`).toString())
+  const buildApps = JSON.parse(fs.readFileSync(`${downloadResponse.downloadPath}/BUILD_APPS/buildApps.json`).toString())
   console.log(buildApps);
+  console.log(buildApps.buildApps);
 })()
 
