35179bf12526c564143144b16a21453a7d8ac24d
Timothy Myers <timmyers09@gmail.com>
2020-03-18 21:48:10 -0600

    fix: build-app config

--- .github/actions/expo/build.sh
+++ .github/actions/expo/build.sh
@@ -7,9 +7,9 @@ echo $EXPO_ANDROID_KEYSTORE_BASE64 > expo-project.jks.base64
 base64 --decode expo-project.jks.base64 > expo-project.jks
 
 sed -i "s/ANDROID_MAPS_API_KEYS/$ANDROID_MAPS_API_KEYS/g" app.json
-pwd
-echo $GITHUB_WORKSPACE
 
+yarn global add gulp-cli turtle-cli@$TURTLE_VERSION
+turtle setup:$PLATFORM
 turtle build:android --keystore-path=./expo-project.jks --keystore-alias=$EXPO_ANDROID_KEYSTORE_ALIAS --public-url=https://expo.brewedhere.co/android-index.json --type=app-bundle --output=brewed-here.aab --config $GITHUB_WORKSPACE/app/app.json
 
 export TURTLE_BUILD=true
