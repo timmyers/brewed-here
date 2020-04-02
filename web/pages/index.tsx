6ee5347961a4f2fccf1519ec17e5281d6da9f50e
Timothy Myers <timmyers09@gmail.com>
2020-04-01 18:03:39 -0600

    Update android link

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -13,9 +13,9 @@ const Home: NextPage = () => (
         <h2>Download the Apps!</h2>
       </header>
       <aside>
-        <a href="https://play.google.com/apps/testing/co.brewedhere.brewedhere" target="_blank"><img alt="Get it on Google Play" src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" height="100"/></a>
+        <a href="https://play.google.com/store/apps/details?id=co.brewedhere.brewedhere" target="_blank"><img alt="Get it on Google Play" src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" height="100"/></a>
         <h3>Android</h3>
-        <p>In open beta testing now!</p>
+        <p>Available now!</p>
       </aside>
       <aside>
         <img alt="Download on the App Store" src="/appstore.svg" height="70"/>
